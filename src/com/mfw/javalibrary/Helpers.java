c24b47c3af6a475e27a509d96ba668ecafa1a00d
MFW2000 <marijnwelmerink@gmail.com>
2022-05-17 18:24:22 +0200

    Refactored entire project

---
+++ src/com/mfw/javalibrary/Helpers.java
@@ -0,0 +1,36 @@
+package com.mfw.javalibrary;
+
+import java.util.Random;
+
+public class Helpers {
+    /**
+     * Trims invalid whitespaces from the given string.
+     * Invalid whitespaces are whitespaces before characters, space after characters
+     * and multiple spaces throughout the string.
+     * @param string string to trim
+     * @return the trimmed string
+     */
+    public static String trimInvalidWhitespaces(String string) {
+        return string.trim().replaceAll(" +", " ");
+    }
+
+    public static int generateRandomInt(int max) {
+        Random random = new Random();
+        return random.nextInt(max);
+    }
+
+    public static int generateRandomInt(int min, int max) {
+        Random random = new Random();
+        return random.nextInt(min, max);
+    }
+
+    public static double generateRandomDouble(double max) {
+        Random random = new Random();
+        return random.nextDouble(max);
+    }
+
+    public static double generateRandomDouble(double min, double max) {
+        Random random = new Random();
+        return random.nextDouble(min, max);
+    }
+}
